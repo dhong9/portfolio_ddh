eba637627645c7b2f7fa4ae2c9980b68c1a8bee9
Daniel Hong <d_hong7@u.pacific.edu>
2021-06-06 11:25:12 -0700

    Added Sudoku puzzle

--- src/views/Components/Sections/SectionCoding.js
+++ src/views/Components/Sections/SectionCoding.js
@@ -10,6 +10,7 @@ import GridItem from "components/Grid/GridItem.js";
 
 import meadow from "assets/img/coding/meadow_solve.gif";
 import connect4 from "assets/img/coding/connect4Anim.gif";
+import sudoku from "assets/img/coding/sudokuAnim.gif";
 
 import styles from "assets/jss/material-kit-react/views/componentsSections/typographyStyle.js";
 
@@ -42,6 +43,16 @@ export default function SectionCoding() {
                 <img
                   src={connect4}
                   alt="Connect four"
+                  className={classes.imgFluid}
+                />
+              </a>
+            </GridItem>
+            <GridItem xs={12} sm={2}>
+              <h4>Sudoku</h4>
+              <a href="https://Sudoku.danielhong.repl.co">
+                <img
+                  src={sudoku}
+                  alt="Sudoku puzzle"
                   className={classes.imgFluid}
                 />
               </a>
